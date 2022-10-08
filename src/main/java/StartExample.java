7c6e7f5eff384d8584c66b6408d1a1539aa8f2b4
Almin <alminkadiric7@gmail.com>
2022-10-08 08:57:30 +0200

    initial commit

--- src/main/java/StartExample.java
+++ src/main/java/StartExample.java
@@ -1,3 +1,18 @@
 public class StartExample {
 
+    public int getHammingDistance (boolean [] array1, boolean [] array2){
+        if(array1.length!= array2.length) {
+            return 0;
+        }
+        int sum=0;
+        for(int i=0; i<array1.length; i++){
+            if(array1[i]!=array2[i]){
+                sum=sum+1;
+            }
+        }
+        return sum;
+
+
+    }
+
 }
--- src/test/java/StartExampleTest.java
+++ src/test/java/StartExampleTest.java
@@ -1,2 +1,25 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class StartExampleTest {
+
+    StartExample example = new StartExample();
+    @Test
+    public void testResult () {
+        boolean [] arr1 = {true,true,true};
+        boolean [] arr2 = {false,false,true};
+        int result=example.getHammingDistance(arr1,arr2);
+        Assertions.assertEquals(2,result);
+
+    }
+    @Test
+    public void testArraysWithSameValues(){
+        boolean [] arr1 = {true,true,true};
+        boolean [] arr2 = {true,true,true};
+        int result=example.getHammingDistance(arr1,arr2);
+        Assertions.assertEquals(0,result);
+
+    }
+
+
 }
